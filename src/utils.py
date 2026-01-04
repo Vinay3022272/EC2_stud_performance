85852fbe5b5c2e761066638b6ec00c20f013a492
Vinay3022272 <ps3022272@kgpian.iitkgp.ac.in>
2026-01-04 21:49:07 +0530

    Initial commit

---
+++ src/utils.py
@@ -0,0 +1,51 @@
+import os
+import sys 
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+from sklearn.metrics import r2_score
+import dill
+from sklearn.model_selection import GridSearchCV
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+        for model_name, model in models.items():
+            param = params[model_name]
+            grid = GridSearchCV(estimator=model, param_grid=param, cv=3, verbose=3)
+            grid.fit(X_train, y_train)
+            
+            best_model = grid.best_estimator_
+            
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
+            r2_score_train = r2_score(y_train, y_train_pred)
+            r2_score_test = r2_score(y_test, y_test_pred)
+            
+            report[model_name] = r2_score_test
+             
+        return report 
+             
+    except Exception as e:
+        raise CustomException(e, sys)    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+          return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+    
